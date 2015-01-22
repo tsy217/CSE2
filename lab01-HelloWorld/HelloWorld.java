a43791e7753c0a0ab5d05900c097a4c4636cd0f0
tsy217 <ytk6895@gmail.com>
2015-01-22 03:37:35 +0000

    updating HelloWorld

--- lab01-HelloWorld/HelloWorld.java
+++ lab01-HelloWorld/HelloWorld.java
@@ -8,13 +8,13 @@ public class HelloWorld{
     //main method
     public static void main(String[] args){
         //print statement
-        System.out.println("***************************");
-        System.out.println("***************************");
-        System.out.println("****                   ****");
-        System.out.println("**** Hello World 2015! ****");
-        System.out.println("****                   ****");
-        System.out.println("***************************");
-        System.out.println("**#************************");
+        System.out.println("lllllllllllllllllllllllllll");
+        System.out.println("ll.......................ll");
+        System.out.println("ll.......................ll");
+        System.out.println("ll...Hello World 2015!...ll");
+        System.out.println("ll.......................ll");
+        System.out.println("ll.......................ll");
+        System.out.println("lllllllllllllllllllllllllll");
 
     }
 }
