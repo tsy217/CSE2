3d38ba47cd87ef9cd6fdd3efe5d8033b4a1f0117
tsy217 <ytk6895@gmail.com>
2015-01-22 03:23:29 +0000

    adding HelloWorld

---
+++ lab01-HelloWorld/HelloWorld.java
@@ -0,0 +1,20 @@
+////////////////////////////
+//Tsu Kuei Yang
+//Lab01
+//Hello World Program
+//
+//defining class
+public class HelloWorld{
+    //main method
+    public static void main(String[] args){
+        //print statement
+        System.out.println("***************************");
+        System.out.println("***************************");
+        System.out.println("****                   ****");
+        System.out.println("**** Hello World 2015! ****");
+        System.out.println("****                   ****");
+        System.out.println("***************************");
+        System.out.println("***************************");
+
+    }
+}
