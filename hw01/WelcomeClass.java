cd732c6701e37e2842d493fc6bea689d53e63599
tsy217 <ytk6895@gmail.com>
2015-01-23 16:32:01 +0000

    upload HW01

--- hw01/WelcomeClass.java
+++ hw01/WelcomeClass.java
@@ -4,7 +4,7 @@
 //Welcome Class Program
 //
 //define class
-public class WellcomeClass{
+public class WelcomeClass{
     //main method
     public static void main(String[] args){
         //printing statemnts
@@ -12,9 +12,9 @@ public class WellcomeClass{
         System.out.println("     |  WELCOME  |    ");
         System.out.println("      -----------     ");
         System.out.println("   ^  ^  ^  ^  ^  ^   ");
-        System.out.println("  / \/ \/ \/ \/ \/ \  ");
+        System.out.println("  / \\/ \\/ \\/ \\/ \\/ \\  ");
         System.out.println(" |-T--S--Y--2--1--7-| ");
-        System.out.println("  \ /\ /\ /\ /\ /\ /  ");
+        System.out.println("  \\ /\\ /\\ /\\ /\\ /\\ /  ");
         System.out.println("   v  v  v  v  v  v   ");
         System.out.println(" CSE002 is about java ");
         System.out.println("  Lets have some fun  ");
