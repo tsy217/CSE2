49dc2d2ec81c265fe9758b25f836e729e0ec5bc9
tsy217 <ytk6895@gmail.com>
2015-01-23 16:13:27 +0000

    updating Hello World

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,23 @@
+///////////////////////////////////////////////////////////////
+//Tsu Kuei Yang
+//Hw01
+//Welcome Class Program
+//
+//define class
+public class WellcomeClass{
+    //main method
+    public static void main(String[] args){
+        //printing statemnts
+        System.out.println("      -----------     ");
+        System.out.println("     |  WELCOME  |    ");
+        System.out.println("      -----------     ");
+        System.out.println("   ^  ^  ^  ^  ^  ^   ");
+        System.out.println("  / \/ \/ \/ \/ \/ \  ");
+        System.out.println(" |-T--S--Y--2--1--7-| ");
+        System.out.println("  \ /\ /\ /\ /\ /\ /  ");
+        System.out.println("   v  v  v  v  v  v   ");
+        System.out.println(" CSE002 is about java ");
+        System.out.println("  Lets have some fun  ");
+
+    }
+}
