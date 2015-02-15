eefda43422d80c38aedfe9c8f6a1997ae9f6a763
tsy217 <ytk6895@gmail.com>
2015-02-15 19:29:14 +0000

    upload hw04

--- hw04/Banking.java
+++ hw04/Banking.java
@@ -1,37 +1,37 @@
-import java.util.Scanner;
-import java.lang.Math;
-public class Banking{
-    public static void main (String[]args){
-        int moneyOwned=(int)((Math.random()*4000)+999);
-        System.out.println("You have $"+moneyOwned+" in your account.");
-        Scanner myScanner=new Scanner(System.in);
-        System.out.println("| 1.Deposit | 2.Withdraw | 3.View Balance|");
-        System.out.print("Enter what you want to do (1, 2 or 3): ");
-        int option=myScanner.nextInt();
-        switch(option){
-            case 1:
-                System.out.print("How much do you want to deposit? ");
-                double deposit=myScanner.nextDouble();
-                if(deposit>0){
-                    System.out.println("You now have $"+(deposit+moneyOwned)+" in your account.");
-                }
-                else{
-                    System.out.println("You enetered an invalid amount of money.");
-                }
-                break;
-            case 2:
-                System.out.print("How much do you want to withdraw? ");
-                double withdraw=myScanner.nextDouble();
-                if(withdraw>0){
-                    System.out.println("You now have $"+(moneyOwned-withdraw)+" in your account.");
-                }
-                else{
-                    System.out.println("You enetered an invalid amount of money.");
-                }
-                break;
-            case 3:
-                System.out.println("You have $"+(moneyOwned)+" in your account.");
-                break;
-        }
-    }
-}
+import java.util.Scanner;//notify compiler Scanner location
+import java.lang.Math;//notify compiler to use lang.Math
+public class Banking{//open class
+    public static void main (String[]args){//open main method
+        int moneyOwned=(int)((Math.random()*4000)+999);//generate random 1000-5000 in person's account
+        System.out.println("You have $"+moneyOwned+" in your account.");//tell user how much money he has
+        Scanner myScanner=new Scanner(System.in);//create scanner
+        System.out.println("| 1.Deposit | 2.Withdraw | 3.View Balance|");//tell user options
+        System.out.print("Enter what you want to do (1, 2 or 3): ");//tell users to only use 1, 2, 3
+        int option=myScanner.nextInt();//store user's option in 'option'
+        switch(option){//open switch statement
+            case 1://case 1 deposit
+                System.out.print("How much do you want to deposit? ");//Ask user how much to deposit
+                double deposit=myScanner.nextDouble();//store amount in 'deposit'
+                if(deposit>0){//open: check if 'deposit' is positive
+                    System.out.println("You now have $"+(deposit+moneyOwned)+" in your account."); //Tell user how much money in account after deposit
+                }//close: if 'deposit' is positie
+                else{//open else: negative or zero
+                    System.out.println("You enetered an invalid amount of money.");// tell user he entered invalid amount of money
+                }//close else: negative or zero
+                break;//break: end case 1
+            case 2://case 2 withdraw
+                System.out.print("How much do you want to withdraw? ");//Ask user how much he/she wants to withdraw
+                double withdraw=myScanner.nextDouble();//store amount in 'withdraw'
+                if(withdraw>0){//open: check if 'withdraw' is positive
+                    System.out.println("You now have $"+(moneyOwned-withdraw)+" in your account.");//Tell user how much money in account after withdraw
+                }//close: if 'withdraw' positive
+                else{//open else: invalid
+                    System.out.println("You enetered an invalid amount of money.");//Tell user he/she entered invalid amount of money
+                }//close else
+                break;//break: end case 2
+            case 3://case 3: tell user how much money he/she has
+                System.out.println("You have $"+(moneyOwned)+" in your account.");//Tell user how much money he/she has
+                break;//end case 3
+        }//close switch
+    }//close main method
+}//close class
--- hw04/WhichNumber.java
+++ hw04/WhichNumber.java
@@ -1,75 +1,75 @@
-import java.util.Scanner;
-public class WhichNumber{
-    public static void main (String[]args){
-        Scanner myScanner=new Scanner(System.in);
-        System.out.println("Think of an integer between 1-10");
+import java.util.Scanner;//tell compiler where to find Scanner
+public class WhichNumber{//open class
+    public static void main (String[]args){//open main method
+        Scanner myScanner=new Scanner(System.in);//create Scanner
+        System.out.println("Think of an integer between 1-10");//prompt user to think of random number b/w 1-10
         System.out.println("I am going to guess the integer.");
-        System.out.print("Is the number even (answer all following questions either y/Y/n/N)? ");
-        String answer1=myScanner.next();
-        if(answer1.equals("y")||answer1.equals("Y")){
-            System.out.print("Is it divisible by 3? ");
-            String answer2=myScanner.next();
-            if(answer2.equals("y")||answer2.equals("Y")){
+        System.out.print("Is the number even (answer all following questions either y/Y/n/N)? ");//ask if it is even: y/Y/n/N
+        String answer1=myScanner.next();//store user's answer
+        if(answer1.equals("y")||answer1.equals("Y")){//open if y or Y
+            System.out.print("Is it divisible by 3? ");//ask if / by 3
+            String answer2=myScanner.next();//store user's answer
+            if(answer2.equals("y")||answer2.equals("Y")){//open if yes for / by 3
               System.out.println("The number is 6");
-            }
-            else if(answer2.equals("n")||answer2.equals("N")){
-                System.out.print("Is it divisible by 4? ");
-                String answer3=myScanner.next();
-                if(answer3.equals("y")||answer3.equals("Y")){
-                    System.out.print("Is the number divided by 4 greater than 1? ");
-                    String answer4=myScanner.next();
-                    if(answer4.equals("y")||answer4.equals("Y")){
+            }//close if yes / by 3
+            else if(answer2.equals("n")||answer2.equals("N")){//open if no for / by 3
+                System.out.print("Is it divisible by 4? ");//ask if / by 4
+                String answer3=myScanner.next();//store answer
+                if(answer3.equals("y")||answer3.equals("Y")){//open if yes for / by 4
+                    System.out.print("Is the number divided by 4 greater than 1? ");//ask if after / by 4, >1
+                    String answer4=myScanner.next();//store answer
+                    if(answer4.equals("y")||answer4.equals("Y")){//open if yes >1
                         System.out.println("The number is 8");
-                    }
-                    else if(answer4.equals("n")||answer4.equals("N")){
+                    }//close if yes >1
+                    else if(answer4.equals("n")||answer4.equals("N")){//open else not >1
                         System.out.println("The number is 4");
-                    }
-                }
-                else if(answer3.equals("n")||answer3.equals("N")){
-                    System.out.print("Is it divisible by 5? ");
-                    String answer5=myScanner.next();
-                    if(answer5.equals("y")||answer5.equals("Y")){
+                    }//close if not >1
+                }//close if yes / by 4
+                else if(answer3.equals("n")||answer3.equals("N")){//open if not for / by 4
+                    System.out.print("Is it divisible by 5? ");//ask user if / by 5
+                    String answer5=myScanner.next();//store answer
+                    if(answer5.equals("y")||answer5.equals("Y")){//open if yes / by 5
                         System.out.println("The number is 10");
-                    }
-                    else if(answer5.equals("n")||answer5.equals("N")){
+                    }//close if yes / by 5
+                    else if(answer5.equals("n")||answer5.equals("N")){//open if not / by 5
                         System.out.println("The nubmer is 2");
-                    }
-                }
-            }
-        }
-        else if(answer1.equals("n")||answer1.equals("N")){
-            System.out.print("Is it divisible by 3? ");
-            String answer6=myScanner.next();
-            if(answer6.equals("y")||answer6.equals("Y")){
-                System.out.print("When divided by 3 is the result greater than 1? ");
-                String answer7=myScanner.next();
-                if(answer7.equals("y")||answer7.equals("Y")){
+                    }//close if not / by 5
+                }//close if not / by 4
+            }//close if not / by 3
+        }//close if yes for even
+        else if(answer1.equals("n")||answer1.equals("N")){//open if no for even
+            System.out.print("Is it divisible by 3? ");//ask if / by 3
+            String answer6=myScanner.next();//store answer
+            if(answer6.equals("y")||answer6.equals("Y")){//open if yes / by 3
+                System.out.print("When divided by 3 is the result greater than 1? ");//ask if after / by 3, >1
+                String answer7=myScanner.next();//store answer
+                if(answer7.equals("y")||answer7.equals("Y")){//open if yes >1
                    System.out.println("The number is 9");
-                }
-                else if(answer7.equals("n")||answer7.equals("N")){
+                }//close if yes >1
+                else if(answer7.equals("n")||answer7.equals("N")){//open if no >1
                     System.out.println("The number is 3");
-                }
-            }
-            else if(answer6.equals("n")||answer6.equals("N")){
-                System.out.print("Is it greater than 6? ");
-                String answer8=myScanner.next();
-                if(answer8.equals("y")||answer8.equals("Y")){
+                }//close if no >1
+            }//close if yes / by 3
+            else if(answer6.equals("n")||answer6.equals("N")){//open if not / by 3
+                System.out.print("Is it greater than 6? ");//ask if >6
+                String answer8=myScanner.next();//store answer
+                if(answer8.equals("y")||answer8.equals("Y")){//open if yes >6
                     System.out.println("The number is 7");
-                }
-                else if(answer8.equals("n")||answer8.equals("N")){
-                    System.out.print("Is it less than 3? ");
-                    String answer9=myScanner.next();
-                    if(answer9.equals("y")||answer9.equals("Y")){
+                }//clsoe if yes >6
+                else if(answer8.equals("n")||answer8.equals("N")){//open if not >6
+                    System.out.print("Is it less than 3? ");//ask if < 3
+                    String answer9=myScanner.next();//store asnwer
+                    if(answer9.equals("y")||answer9.equals("Y")){//open if yes < 3
                         System.out.println("The number is 1");
-                    }
-                    else if(answer9.equals("n")||answer9.equals("N")){
+                    }//close if yes < 3
+                    else if(answer9.equals("n")||answer9.equals("N")){//open if not < 3
                         System.out.println("The number is 5");
-                    }
-                }
-            }
-        }
-        else{
+                    }//close if not < 3
+                }//clsoe if not >6
+            }//clsoe if not / by 3
+        }//close if no for even
+        else{//open if incorrect input
             System.out.println("*Invalid input* Please enter y or Y or n or N!");
-        }
-    }
-}
+        }//close if incorrect input
+    }//close main method
+}//close class
