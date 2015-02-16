5f237403e68d0fc665b93cc6941f58d4a6ad251a
tsy217 <ytk6895@gmail.com>
2015-02-16 18:23:14 +0000

    upload hw04

--- hw04/WhichNumber.java
+++ hw04/WhichNumber.java
@@ -10,7 +10,14 @@ public class WhichNumber{//open class
             System.out.print("Is it divisible by 3? ");//ask if / by 3
             String answer2=myScanner.next();//store user's answer
             if(answer2.equals("y")||answer2.equals("Y")){//open if yes for / by 3
-              System.out.println("The number is 6");
+              System.out.println("Is your number 6? ");
+              String finale=myScanner.next();
+              if(finale.equals("y")||finale.equals("Y")){
+                  System.out.println("I knew it! I am right!");
+              }
+              else if(finale.equals("n")||finale.equals("N")){
+                  System.out.println("You are lying, don't lie.");
+              }
             }//close if yes / by 3
             else if(answer2.equals("n")||answer2.equals("N")){//open if no for / by 3
                 System.out.print("Is it divisible by 4? ");//ask if / by 4
@@ -19,20 +26,48 @@ public class WhichNumber{//open class
                     System.out.print("Is the number divided by 4 greater than 1? ");//ask if after / by 4, >1
                     String answer4=myScanner.next();//store answer
                     if(answer4.equals("y")||answer4.equals("Y")){//open if yes >1
-                        System.out.println("The number is 8");
+                        System.out.println("Is your number 8? ");
+                        String finale=myScanner.next();
+                        if(finale.equals("y")||finale.equals("Y")){
+                            System.out.println("I knew it! I am right!");
+                        }
+                        else if(finale.equals("n")||finale.equals("N")){
+                            System.out.println("You are lying, don't lie.");
+                        }
                     }//close if yes >1
                     else if(answer4.equals("n")||answer4.equals("N")){//open else not >1
-                        System.out.println("The number is 4");
+                        System.out.println("Is the number 4? ");
+                        String finale=myScanner.next();
+                        if(finale.equals("y")||finale.equals("Y")){
+                            System.out.println("I knew it! I am right!");
+                        }
+                        else if(finale.equals("n")||finale.equals("N")){
+                            System.out.println("You are lying, don't lie.");
+                        }
                     }//close if not >1
                 }//close if yes / by 4
                 else if(answer3.equals("n")||answer3.equals("N")){//open if not for / by 4
                     System.out.print("Is it divisible by 5? ");//ask user if / by 5
                     String answer5=myScanner.next();//store answer
                     if(answer5.equals("y")||answer5.equals("Y")){//open if yes / by 5
-                        System.out.println("The number is 10");
+                        System.out.println("Is the number 10? ");
+                        String finale=myScanner.next();
+                        if(finale.equals("y")||finale.equals("Y")){
+                            System.out.println("I knew it! I am right!");
+                        }
+                        else if(finale.equals("n")||finale.equals("N")){
+                            System.out.println("You are lying, don't lie.");
+                        }
                     }//close if yes / by 5
                     else if(answer5.equals("n")||answer5.equals("N")){//open if not / by 5
-                        System.out.println("The nubmer is 2");
+                        System.out.println("Is the nubmer is 2");
+                        String finale=myScanner.next();
+                        if(finale.equals("y")||finale.equals("Y")){
+                            System.out.println("I knew it! I am right!");
+                        }
+                        else if(finale.equals("n")||finale.equals("N")){
+                            System.out.println("You are lying, don't lie.");
+                        }
                     }//close if not / by 5
                 }//close if not / by 4
             }//close if not / by 3
@@ -44,26 +79,61 @@ public class WhichNumber{//open class
                 System.out.print("When divided by 3 is the result greater than 1? ");//ask if after / by 3, >1
                 String answer7=myScanner.next();//store answer
                 if(answer7.equals("y")||answer7.equals("Y")){//open if yes >1
-                   System.out.println("The number is 9");
+                   System.out.println("Is the number 9?");
+                   String finale=myScanner.next();
+                   if(finale.equals("y")||finale.equals("Y")){
+                       System.out.println("I knew it! I am right!");
+                    }
+                    else if(finale.equals("n")||finale.equals("N")){
+                        System.out.println("You are lying, don't lie.");
+                    }
                 }//close if yes >1
                 else if(answer7.equals("n")||answer7.equals("N")){//open if no >1
-                    System.out.println("The number is 3");
+                    System.out.println("Is the number 3?");
+                    String finale=myScanner.next();
+                    if(finale.equals("y")||finale.equals("Y")){
+                        System.out.println("I knew it! I am right!");
+                    }
+                    else if(finale.equals("n")||finale.equals("N")){
+                        System.out.println("You are lying, don't lie.");
+                    }
                 }//close if no >1
             }//close if yes / by 3
             else if(answer6.equals("n")||answer6.equals("N")){//open if not / by 3
                 System.out.print("Is it greater than 6? ");//ask if >6
                 String answer8=myScanner.next();//store answer
                 if(answer8.equals("y")||answer8.equals("Y")){//open if yes >6
-                    System.out.println("The number is 7");
+                    System.out.println("Is the number 7?");
+                    String finale=myScanner.next();
+                        if(finale.equals("y")||finale.equals("Y")){
+                            System.out.println("I knew it! I am right!");
+                        }
+                        else if(finale.equals("n")||finale.equals("N")){
+                            System.out.println("You are lying, don't lie.");
+                        }
                 }//clsoe if yes >6
                 else if(answer8.equals("n")||answer8.equals("N")){//open if not >6
                     System.out.print("Is it less than 3? ");//ask if < 3
                     String answer9=myScanner.next();//store asnwer
                     if(answer9.equals("y")||answer9.equals("Y")){//open if yes < 3
-                        System.out.println("The number is 1");
+                        System.out.println("Is the number 1?");
+                        String finale=myScanner.next();
+                        if(finale.equals("y")||finale.equals("Y")){
+                            System.out.println("I knew it! I am right!");
+                        }
+                        else if(finale.equals("n")||finale.equals("N")){
+                            System.out.println("You are lying, don't lie.");
+                        }
                     }//close if yes < 3
                     else if(answer9.equals("n")||answer9.equals("N")){//open if not < 3
-                        System.out.println("The number is 5");
+                        System.out.println("Is the number 5?");
+                        String finale=myScanner.next();
+                        if(finale.equals("y")||finale.equals("Y")){
+                            System.out.println("I knew it! I am right!");
+                        }
+                        else if(finale.equals("n")||finale.equals("N")){
+                            System.out.println("You are lying, don't lie.");
+                        }
                     }//close if not < 3
                 }//clsoe if not >6
             }//clsoe if not / by 3
